7dbb0f5ade6cb74c5f50307d02c8ed9db3ac2c6f
Aidan Neal <aidanneal4@gmail.com>
2022-07-27 05:51:09 -0400

    comment code

--- src/drive.c
+++ src/drive.c
@@ -1,49 +1,83 @@
 #include "drive.h"
 
+/**
+ * Opens the drive for writing, allocates 512 bytes of memory and initializes it
+ * to 0, and writes 512 bytes of zeros to the drive
+ *
+ * @param drive The drive that you want to write zeros to.
+ *
+ * @return 0.
+ */
 int write_zeros_512(char *drive) {
+  /* Opening the drive for writing. */
   int fd = open(drive, O_WRONLY);
+  /* Allocating 512 bytes of memory and initializing it to 0. */
   char *zeros = calloc(512, sizeof(char));
   ssize_t written = 0;
 
+  /* Checking if the file descriptor is less than 0. If it is, it prints an
+  error message and returns -1. */
   if (fd < 0) {
     perror("open");
     return -1;
   }
 
+  /* Checking if the calloc function failed. If it did, it prints an error
+   * message and returns -1. */
   if (zeros == NULL) {
     perror("calloc");
     return -1;
   }
+
+  /* Writing 512 bytes of zeros to the drive. */
   do {
     written = write(fd, zeros, 512);
-  } while (written == 512);
+  } while (written ==
+           512); /* Checking if the number of bytes written is equal to 512. If
+it is, it will continue to write 512 bytes of zeros to the drive. */
 
+  /* Close the file descriptor. */
   close(fd);
+
+  /* Freeing the memory that was allocated by the calloc function. */
   free(zeros);
   return 0;
 }
 
-int write_zeros_512_verbose(char *drive)
-{
+int write_zeros_512_verbose(char *drive) {
+  /* Opening the drive for writing. */
   int fd = open(drive, O_WRONLY);
+  /* Allocating 512 bytes of memory and initializing it to 0. */
   char *zeros = calloc(512, sizeof(char));
   ssize_t written, total = 0;
 
+  /* Checking if the file descriptor is less than 0. If it is, it prints an
+  error message and returns -1. */
   if (fd < 0) {
     perror("open");
     return -1;
   }
 
+  /* Checking if the calloc function failed. If it did, it prints an error
+  message and returns -1. */
   if (zeros == NULL) {
     perror("calloc");
     return -1;
   }
-  do {
-    total += written = write(fd, zeros, 512);
-    printf("\nBytes written: %ld", total);
-  } while (written == 512);
 
+  do {
+    /* Adding the number of bytes written to the total number of bytes
+    written. */
+    total += written = write(fd, zeros, 512);
+    /* Printing the total number of bytes written to the drive. */
+    printf("\nBytes written: %ld", total);
+  } while (written == 512); /* Checking if the number of bytes written is equal to 512. If it is,
+  it will continue to write 512 bytes of zeros to the drive. */
+
+  /* Close the file descriptor. */
   close(fd);
+
+  /* Freeing the memory that was allocated by the calloc function. */
   free(zeros);
   return 0;
 }
--- src/main.c
+++ src/main.c
@@ -2,24 +2,40 @@
 #include "drive.h"
 
 int confirm(char *message) {
+  /* Allocating memory for a single character. */
   char *answer = calloc(1, sizeof(char));
-  while (1) {
+  while (1) { /* An infinite loop. */
+    /* Asking the user to input a string and then storing it in the variable
+     * `answer`. */
     printf("%s (y/n): ", message);
     scanf("%s", answer);
+
+    /* Comparing the string `answer` to the string `y`. If they are equal, it
+     * returns 1. */
     if (strcmp(answer, "y") == 0) {
       free(answer);
       return 1;
-    } else if (strcmp(answer, "n") == 0) {
+    }
+
+    /* Comparing the string `answer` to the string `n`. If they are equal, it
+       returns 0. */
+    else if (strcmp(answer, "n") == 0) {
       free(answer);
       return 0;
-    } else {
+    }
+
+    /* Printing an error message if the user enters an invalid answer. */
+    else {
       printf("Invalid answer.\n");
     }
   }
 }
+
 int main(int argc, char *argv[]) {
   int opt;
   int verbose, help = 0;
+
+  /* Parsing the command line arguments. */
   while ((opt = getopt(argc, argv, "hv")) != -1) {
     switch (opt) {
     case 'v':
@@ -31,46 +47,59 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  /* Checking if the user has entered a command line argument. If they haven't,
+  it prints the usage message and exits. */
   if (argc < 2 || help) {
     printf("Usage: %s <drive|partiton> (/dev/sdc or /dev/sdc1)\n", argv[0]);
     return 1;
   }
 
+  /* Creating an array of strings. */
   char *menu_items[WIPE_MENU_SIZE] = {"Write zeros to drive", "Exit"};
 
+  /* Printing the menu items. */
   for (int i = 0; i < WIPE_MENU_SIZE; i++) {
     printf("%s [%d]\n", menu_items[i], i);
   }
 
+  /* Asking the user to enter a menu choice. */
   int menu_choice = 1;
-  printf ("Enter menu choice: ");
+  printf("Enter menu choice: ");
   scanf("%d", &menu_choice);
 
+  /* Checking if the user has entered a valid menu choice. */
   if (menu_choice != 0 && menu_choice != 1) {
     printf("Invalid menu choice.\n");
     return 1;
   }
 
   if (menu_choice == 0) {
-    if (confirm("Are you sure you want to wipe the drive?")) {
-      if (verbose == 1 ) {
-        printf("Writing 0s to %s\n", argv[2]);
-        if (write_zeros_512_verbose(argv[2]) == 0) {
-          printf("Wipe successful.\n");
-        } else {
-          printf("Wipe failed.\n");
-        }
-      } else {
-         printf("Writing 0s to %s (Use -v to see progress)\n", argv[1]);
-        if (write_zeros_512(argv[1]) == 0) {
-          printf("Wipe successful.\n");
-        } else {
-          printf("Wipe failed.\n");
-        }
-      }
-
-    } else {
+    /* Asking the user if they are sure they want to wipe the drive. If
+    they say no, it prints "Wipe cancelled." */
+    if (!confirm("Are you sure you want to wipe the drive?")) {
       printf("Wipe cancelled.\n");
+      return 0;
+    }
+
+    /* Printing the string "Writing 0s to " and then the value of the
+    variable `argv[2]`. */
+    printf("Writing 0s to %s\n", argv[2]);
+
+
+    if (verbose == 1) { /* Checking if the user has entered the `-v` command line argument. */
+
+      if (write_zeros_512_verbose(argv[2]) == 0) {
+        printf("Wipe successful.\n");
+      } else {
+        printf("Wipe failed.\n");
+        return 1;
+      }
+    } else {
+      if (write_zeros_512(argv[1]) == 0) {
+        printf("Wipe successful.\n");
+      } else {
+        printf("Wipe failed.\n");
+      }
     }
   }
 }
